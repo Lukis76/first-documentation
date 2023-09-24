e0049749b5f4872b899ef6e8ed862f2d71ca0c04
Lucas Ruben Manoguerra <kiu.manoguerra@gmail.com>
2023-09-24 16:33:47 -0300

    astro config and clipboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,47 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
+import remarkCodeTitles from "remark-code-titles";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noreferrer noopener"],
+          content: {
+            type: "text",
+            value: "↗",
+          },
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: "heading-link heading-link--hidden---effects",
+            "data-heading-link": true,
+          },
+          behavior: "wrap",
+        },
+      ],
+    ],
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: "material-theme-darker",
+      wrap: true,
+    },
+  },
   integrations: [mdx(), sitemap(), tailwind()]
 });
